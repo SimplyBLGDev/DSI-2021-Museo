2278e8436e9246b6462de4ebc520df384a7eaa68
frandacos <63423347+frandacos@users.noreply.github.com>
2021-06-14 22:01:49 -0300

    Interfaz primera parte

--- DSI2021/Frontend/MainMenu.cs
+++ DSI2021/Frontend/MainMenu.cs
@@ -7,11 +7,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DSI2021.Frontend;
 
 namespace DSI2021 {
 	public partial class MainMenu : Form {
 		public MainMenu() {
 			InitializeComponent();
 		}
-	}
+
+        private Form activeForm = null;
+        private void openChildForm(Form childForm)
+        {
+            if (activeForm != null)
+                activeForm.Close();
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            pnlPrincipal.Controls.Add(childForm);
+            pnlPrincipal.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+        }
+
+        private void btnRegistrarVentaEntradas_Click(object sender, EventArgs e)
+        {
+            openChildForm(new PantallaRegistroVenta());
+        }
+
+    }
 }
